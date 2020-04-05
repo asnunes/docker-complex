5a6a9e24e1dc3071b653a105013b9abe4cd60cf6
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-04-05 01:51:47 -0300

    Fix client state

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,59 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Fib() {
   const [seenIndexes, setSeenIndexes] = useState([]);
   const [values, setValues] = useState({});
-  const [index, setIndex] = useState('');
+  const [index, setIndex] = useState("");
 
   useEffect(() => {
     fetchValues();
     fetchIndexes();
   }, []);
 
-
   async function fetchValues() {
-    const values = await axios.get('/api/values/current');
+    const values = await axios.get("/api/values/current");
     setValues(values.data);
   }
 
   async function fetchIndexes() {
-    const seenIndexes = await axios.get('/api/values/all');
-    setSeenIndexes(seenIndexes.data);
+    const seenIndexes = await axios.get("/api/values/all");
+    setSeenIndexes(seenIndexes.data.map(({ number }) => number));
   }
 
   async function onSubmit(event) {
     event.preventDefault();
-    await axios.post('api/values', { index });
-    setIndex('');
+    await axios.post("api/values", { index });
+    setIndex("");
   }
 
   return (
     <>
       <form onSubmit={onSubmit}>
         <label>Enter your index:</label>
-        <input
-          value={index}
-          onChange={e => setIndex(e.target.value)}
-        />
+        <input value={index} onChange={(e) => setIndex(e.target.value)} />
         <button>Submit</button>
       </form>
       <h3>Indexes I have seen:</h3>
-        {seenIndexes.join(', ')}
+      {seenIndexes.join(", ")}
       <h3>Calculated Values:</h3>
-        <Values values={values}/>
+      <Values values={values} />
     </>
-  )
+  );
 }
 
 function Values({ values }) {
-  function getEntries(values) {
-    values.map((key) => (
-      <div key={key}>
-        For index {key}, I calculated values[key]
-      </div>
-    ));
-  }
+  return (
+    <>
+      {Object.keys(values).map((key) => (
+        <div key={key}>For index {key}, I calculated {values[key]}</div>
+      ))}
+    </>
+  );
 }
 
 export default Fib;
--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default () => {
+export default function() {
   return (
     <div>
       I'm some other page...
